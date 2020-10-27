c8c9dfa8c53c40f45b49136f707faccb5c1fd0e0
Romain Testard <romain.testard@bluenove.com>
2020-10-27 20:01:52 +0100

    endpoint for creating users

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class AdminSiteTests(TestCase):
 
     def setUp(self):
@@ -17,7 +18,6 @@ class AdminSiteTests(TestCase):
             name='Test User Full Name',
         )
 
-    
     def test_users_listed(self):
         """Test that users are listed on the user page"""
         url = reverse('admin:core_user_changelist')
